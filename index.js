0400955a731770d002966cd43326f232f16d2067
schoes <sven.schoeni@gmail.com>
2020-03-11 08:27:08 +0100

    1 file modified

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ const label = tf.tensor([[200, 230, 240, 345]]);
 
 const predictionPoint = tf.tensor([123,35]);
 
-featurees.sub(predictionPoint).pow(2).sum(1).pow(.5).print()
+const distances = featurees.sub(predictionPoint).pow(2).sum(1).pow(.5).expandDims(1);
+
+distances.concat(label,1).print()
